4d49db19f5c3cd320efcb7c5f408842c3758ea52
kench1994 <kench.h.wong@gmail.com>
2022-12-09 10:33:44 +0800

    new post

--- new-post.lua
+++ new-post.lua
@@ -8,11 +8,11 @@ function Concat(...)
     return message
 end
 
-local date = os.date("%Y-%m-%d");
+local date_yesterday = os.date("%Y-%m-%d", os.time() - 24*60*60);
 local time = os.date("%H:%M:%S");
 
 local current_path = string.sub(debug.getinfo(1).source, 2, string.len("new-post.lua") * -1 - 1)
-local post_path = Concat(current_path, "_posts/", date, "-post.md")
+local post_path = Concat(current_path, "_posts/", date_yesterday, "-.md")
 
 
 local file = io.open(post_path, "w+")
@@ -20,7 +20,7 @@ local file = io.open(post_path, "w+")
 file:write("---\n")
 file:write("layout: post\n")
 file:write("title:  \n")
-file:write("date:   ", date, " ", time, "\n")
+file:write("date:   ", date_yesterday, " ", time, "\n")
 file:write("categories: \n")
 file:write("tags: \n")
 file:write("---\n\n")
